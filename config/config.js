df670f6dda66b00564bf6624d7725a016f613052
Mario ANDRIANARILIVA <mario.andrianariliva@axian-group.com>
2020-02-23 09:45:16 +0100

    skeleton mern webpack babel

---
+++ config/config.js
@@ -0,0 +1,9 @@
+const config = {
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 3000, 
+    jwtSecret: process.env.JWI_SECRET || "YOUR_secret_key",
+    mongoUri: process.env.MONGO_URI || process.env.MONGO_HOST ||
+            'mongodb://' + (process.env.IP || 'localhost') + ':' + (process.env.MONGO_PORT ||'27017') + '/mernprojet'
+}
+
+export default config
